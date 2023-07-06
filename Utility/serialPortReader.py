5218a14441f213acbbe26f296e3487f4f69e7e56
SafonovVladimir <vladimir_safonov@ukr.net>
2023-07-06 11:59:04 +0300

    used while self._is_running except while True

--- Utility/serialPortReader.py
+++ Utility/serialPortReader.py
@@ -10,11 +10,12 @@ class SerialPortReader(QThread):
         self.port = port
         self.baudrate = baudrate
         self.serial_port = None
+        self._is_running = True
 
     def run(self):
         self.serial_port = serial.Serial(self.port, self.baudrate)
 
-        while True:
+        while self._is_running:
             data = self.serial_port.readline().strip().decode()
             if "HUB" in data:
                 decoded_data = data[7:21] + data[29:]
